dc36c15ed996e1d0954f178a2b307ed79d8564a0
Adarsh khare <38292989+AdarshKhare29@users.noreply.github.com>
2025-05-02 14:05:01 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,12 +19,11 @@ const Home = () => {
                         <div className="col-md-8">
                             <p>
                                Hi, I'm Adarsh Khare — a passionate Frontend Developer and Computer Science graduate with 5 years of experience building intuitive, scalable, and high-performance web applications.
-
-I specialize in React.js and JavaScript, and I bring a strong eye for design, a deep understanding of UI/UX principles, and a knack for clean, maintainable code. From startups to enterprise projects, I’ve led the development of solutions that drive real business value.
-
-I thrive in fast-paced, collaborative environments, and I’m skilled at managing the entire software development lifecycle, from planning and design to deployment and optimization. Whether it’s solving complex problems or coordinating cross-functional teams, I always aim to deliver on time — without compromising quality.
-
-If you’re looking for someone who combines technical expertise with a problem-solving mindset, let’s connect! I'm always open to new opportunities, challenges, or just a great conversation.
+                               I specialize in React.js and JavaScript, and I bring a strong eye for design, a deep understanding of UI/UX principles, and a knack for clean, maintainable code. From startups to 
+                               enterprise projects, I’ve led the development of solutions that drive real business value.I thrive in fast-paced, collaborative environments, and I’m skilled at managing the entire 
+                               software development lifecycle, from planning and design to deployment and optimization. Whether it’s solving complex problems or coordinating cross-functional teams, I always aim to 
+                               deliver on time — without compromising quality.If you’re looking for someone who combines technical expertise with a problem-solving mindset, let’s connect! I'm always open to new 
+                               opportunities, challenges, or just a great conversation.
                             </p>
                         </div>
                     </div>
